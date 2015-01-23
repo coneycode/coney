7f9ed62c11d0c865b70086212a9920178b592c4a
tufu9441 <tufu9441@gmail.com>
2015-01-23 23:12:40 +0800

    返回顶部小火箭优化

--- source/js/totop.js
+++ source/js/totop.js
@@ -1,26 +1,12 @@
-(function($) {
-  // When to show the scroll link
-  // higher number = scroll link appears further down the page
-  var upperLimit = 500;
-
-  // Our scroll link element
-  var scrollElem = $('#totop');
-
-  // Scroll to top speed
-  var scrollSpeed = 500;
-
-  // Show and hide the scroll to top link based on scroll position
-  scrollElem.hide();
-  $(window).scroll(function () {
-    var scrollTop = $(document).scrollTop();
-    if ( scrollTop > upperLimit ) {
-      $(scrollElem).stop().fadeTo(300, 1); // fade back in
-    }else{
-      $(scrollElem).stop().fadeTo(300, 0); // fade out
-    }
-  });
-  // Scroll to top animation on click
-  $(scrollElem).click(function(){
-    $('html, body').animate({scrollTop:0}, scrollSpeed); return false;
-  });
-})(jQuery);
+$(window).scroll(function() {
+        $(window).scrollTop() > 100 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
+    });
+    $("#rocket").click(function() {
+        $("#rocket").addClass("launch");
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600, function() {
+            $("#rocket").removeClass("show launch");
+        });
+        return false;
+    });
